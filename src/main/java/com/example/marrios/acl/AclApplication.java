c283c062c8d7797285c935b733b1fa989960d182
edmariooliver <edmariooliver@Outlook.com>
2022-09-04 04:57:21 -0300

    init

---
+++ src/main/java/com/example/marrios/acl/AclApplication.java
@@ -0,0 +1,13 @@
+package com.example.marrios.acl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AclApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AclApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/marrios/acl/AclApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.marrios.acl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AclApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
